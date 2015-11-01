0b4e1bb4dd0dc3cfb4fe82f8fd146c2494e9ad27
Yukai Zhang <yukai.zhang13@imperial.ac.uk>
2015-11-01 15:20:39 +0000

    something to start with, still don't know how to include gameplay.js, still writing in .jade file

---
+++ public/js/gameplay.js
@@ -0,0 +1,19 @@
+var renderer = new PIXI.CanvasRenderer(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Stage;
+
+var zombieTexture = PIXI.Texture.fromImage('zombie.png');
+var zombie = new PIXI.Sprite(zombieTexture);
+
+zombie.position.x = window.innerWidth / 2 - 150;
+zombie.position.y = window.innerHeight / 2 - 150;
+
+stage.addChild(zombie);
+
+function draw() {
+  renderer.render(stage);
+  requestAnimationFrame(draw);
+}
+draw();
