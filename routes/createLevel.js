648416f7f26c9f7acdde2b8fc489fd882471b06c
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2015-11-06 12:35:50 +0000

    fixed bug for display username

--- routes/createLevel.js
+++ routes/createLevel.js
@@ -5,7 +5,13 @@ var levelDatas = require('../models/levelData');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   require('connect-ensure-login').ensureLoggedIn();
-  res.render('createLevel', { title: 'Create', uname: req.user.displayName});
+    var displayName = "";
+    if(!req.user) {
+        displayName = undefined;
+    } else {
+        displayName = req.user.displayName;
+    }
+  res.render('createLevel', { title: 'Create', uname: displayName, displayName:displayName});
 });
 
 module.exports = router;
