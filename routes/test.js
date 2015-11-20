45dbd878bd45fb4877f8db5ffe8972f3a61871c2
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2015-11-20 12:03:16 +0000

    ready to merge

--- routes/test.js
+++ routes/test.js
@@ -6,6 +6,30 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   require('connect-ensure-login').ensureLoggedIn();
   res.send('hello world'+ req.param('a2')); // response string with parameter sent
+  /*
+// Save json format level data to mongo db
+levelDatas.levelData.count({}, function( err, count){ // insert record
+  console.log( "Number of levels:", count );
+  levelDatas.levelData.create({
+  id: count+1,
+  data: {
+  "dimen": 5,
+  "start":[{"Coor":"1,0", "Dir":"1"}],
+  "end":[{"Coor":"5,6", "Dir":"3"}],
+
+  "straight": 5,
+  "endPoint": 5,
+  "threeWay": 5,
+  "turn": 5,
+
+  "snake":[{"Coor":"2,2", "Dir":"0"}],
+  "tree":[{"Coor":"3,3", "Dir":"0"}]
+  }
+  }).then(function(levelData) {
+      console.log(" id generated is " + user.id);
+  });
+})
+  */
 });
 
 module.exports = router;
