a2dc98a81399fd1314562dda52b8b30c3fdb7449
Yukai Zhang <yukai.zhang13@imperial.ac.uk>
2016-01-05 22:56:49 +0800

    added basic tests for local host

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
 module.exports = {
-'Page title is correct': function (test) {
-  test
-    .open('http://google.com')
-    .assert.title().is('Google', 'It has title')
-    .done();
-}
+
+	'Test for local host': function (test){
+		test
+			.open('http://localhost:8080')
+			.assert.title().is('Welcome','title is right')
+			.assert.exists('#jump_to_home','exist home page')
+			.click('#jump_to_home')
+			.assert.exists('#play','exist play page')
+			.click('#play')
+			.done();
+		test.screenshot('screenshots/final.png');
+	}
 };
