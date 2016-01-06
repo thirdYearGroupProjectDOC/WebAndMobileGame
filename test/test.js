23776aba314044061d2b535bcf6d121aa610fec4
Yukai Zhang <yukai.zhang13@imperial.ac.uk>
2016-01-06 12:31:10 +0800

    adding id's in view for testing reasongs, wrote basic test for signups

--- test/test.js
+++ test/test.js
@@ -5,9 +5,30 @@ module.exports = {
 			.open('http://localhost:8080')
 			.assert.title().is('Welcome','title is right')
 			.assert.exists('#jump_to_home','exist home page')
+			.screenshot('screenshots/welcome page.png')
+
+     		//check home page have expected buttons
 			.click('#jump_to_home')
 			.assert.exists('#play','exist play page')
+			.assert.exists('#signin','exist signin button')
+			.assert.exists('#signup','exist signup button')
+			.assert.doesntExist('#createLevel','not login shoudnt be able to create level')			
+			.screenshot('screenshots/homepage.png')
+
+			//check when select levels tehre are no unexpected buttons when not logged in
 			.click('#play')
+			.assert.doesntExist('#createLevel','not login shoudnt be able to create level')
+			.assert.doesntExist('logout','cant logout when not logged in')
+			.screenshot('screenshots/levels_not logged in.png')
+
+			// assume admin is an existing account
+			.click('#signin')
+			.type('#login_usr', 'admin')
+    		.type('#login_pss', 'passwd')
+    		.click('#login_submit')
+    		.assert.title().is('Select Levels','title is select levels')
+    		.assert.exists('#createLevel')
+
 			.done();
 		test.screenshot('screenshots/final.png');
 	}
